3af5938ae5c2d72af16a52ecd2ade2285b4798d5
XavierJ4 <xrodriguez9626@arecibointer.edu>
2025-04-23 15:02:20 -0400

    updated

--- Mod-1-Lec-2/app.py
+++ Mod-1-Lec-2/app.py
@@ -2,17 +2,17 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/info", methods=["GET"])
 def home(): 
     return "Bienvenidos a mi API"
 
-@app.route("/saludo", methods=["POST"])
+@app.route("/mensaje", methods=["POST"])
 def saludo():
     
     data = request.json
     nombre = data.get("nombre", "Usuario")
-    
-    return f"Hola, {nombre}!"
+
+    return f"Hola, min nombre es {nombre} y este es una ruta de mi server de flask!"
 
 if __name__ == "__main__":
     app.run(debug=True)
